855e278a6fdb0fd384b7717940d138d1df7906ed
sukriti-paudel <sukrititwozero@gmail.com>
2024-12-04 11:07:33 +0545

    To do list using context api

---
+++ todolist/src/context/Todocontext.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+export const ToDoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "ToDo message",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => { },
+    updateTodo: (id, todo) => { },
+    deleteTodo: (id) => { },
+    toggleComplete: (id) => { }
+})
+
+export const useTodo = () => {
+    return useContext(ToDoContext)
+}
+export const TodoProvider = ToDoContext.Provider
